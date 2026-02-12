06f25862d7c9f07de5b23015ff88c0b489aff954
Nikita <youungstepanov@gmail.com>
2026-02-12 06:52:02 +0300

    init_remove_gradient_blur

--- components/themed-view.tsx
+++ components/themed-view.tsx
@@ -1,18 +1,14 @@
 import type { ViewProps } from 'react-native';
-import { StyleSheet, useColorScheme, View } from 'react-native';
-import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 import { useThemeColor } from '@/hooks/use-theme-color';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { TopSafeAreaGradient } from '@/components/top-safe-area-gradient';
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
   isModal?: boolean;
   addPadding?: number;
-  isBluer?: boolean;
-  isGradient?: boolean;
 };
 
 export function ThemedView({
@@ -20,20 +16,10 @@ export function ThemedView({
   darkColor,
   isModal = false,
   addPadding = 16,
-  isBluer = true,
-  isGradient = true,
   children,
 }: ThemedViewProps) {
-  const colorScheme = useColorScheme();
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
   const insets = useSafeAreaInsets();
-  const scrollY = useSharedValue(0);
-
-  const onScroll = useAnimatedScrollHandler({
-    onScroll: (event) => {
-      scrollY.value = event.contentOffset.y;
-    },
-  });
 
   const safeAreaPadding = {
     paddingTop: insets.top + addPadding,
@@ -43,21 +29,12 @@ export function ThemedView({
   };
 
   const paddingStyle = isModal ? { padding: addPadding } : safeAreaPadding;
-  const topOverlayHeight = insets.top + addPadding;
 
   return (
     <View style={[styles.container, { backgroundColor }]}>
-      <Animated.ScrollView onScroll={onScroll} scrollEventThrottle={16} contentContainerStyle={paddingStyle}>
+      <ScrollView contentContainerStyle={[paddingStyle]}>
         {children}
-      </Animated.ScrollView>
-      {isGradient && !isModal && insets.top > 0 ? (
-        <TopSafeAreaGradient
-          height={topOverlayHeight}
-          scheme={colorScheme === 'dark' ? 'dark' : 'light'}
-          scrollY={scrollY}
-          enableBlur={isBluer}
-        />
-      ) : null}
+      </ScrollView>
     </View>
   );
 }
